63f56351478e694b3de5d6646b31d65dc5cfa823
Daniel <daniel.warnelov@garo.se>
2025-02-19 00:17:26 +0100

    add movement in all directions

--- src/game.py
+++ src/game.py
@@ -31,10 +31,20 @@ while command.casefold() not in ["q", "x"]:
     command = input("Use WASD to move, Q/X to quit. ")
     command = command.casefold()[:1]
 
-    if command == "d" and player.can_move(1, 0, g):  # move right
-        # TODO: skapa funktioner, så vi inte behöver upprepa så mycket kod för riktningarna "W,A,S"
-        maybe_item = g.get(player.pos_x + 1, player.pos_y)
-        player.move(1, 0)
+    # Command = Move player
+    if command.casefold() in "wasd":
+        command_to_movement = {
+            "w": (0, -1),  # Move up
+            "a": (-1, 0),  # Move left
+            "s": (0, 1),   # Move down
+            "d": (1, 0),   # Move right
+        }
+        move_x, move_y = command_to_movement[command]
+    
+        maybe_item = None
+        if player.can_move(move_x, move_y, g):
+            maybe_item = g.get(player.pos_x + move_x, player.pos_y + move_y)
+            player.move(move_x, move_y)
 
         if isinstance(maybe_item, pickups.Item):
             # we found something
